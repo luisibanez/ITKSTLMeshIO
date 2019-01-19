7d907f16a468b7f3986937f47e0eaf03298416ba
Matt McCormick <matt.mccormick@kitware.com>
2019-01-19 17:30:51 -0500

    ENH: Python package requires itk>4
    We build against ITK 5.

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     keywords='ITK InsightToolkit STL Mesh',
     url=r'https://github.com/InsightSoftwareConsortium/ITKIOMeshSTL',
     install_requires=[
-        r'itk'
+        r'itk>4'
     ]
     )
